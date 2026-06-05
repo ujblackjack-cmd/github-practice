c5da752ef6742c2cafa604761299c8085074d189
ujblackjack-cmd <ujblackjack@gmail.com>
2026-06-05 14:15:06 +0900

    Add calculator with add and substract

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# calculator.py
+def add(a, b):
+    """두 수를 더한다."""
+    return a + b
+ 
+ 
+def subtract(a, b):
+    """두 수를 뺀다."""
+    return a - b
+ 
+ 
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("5 - 1 =", subtract(5, 1))
